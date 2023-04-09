95039583f5382668bb2b832acb98379db094df6b
Kivi <kivi0103@163.com>
2023-04-09 22:55:13 +0800

    了解了感知器原理，自己跟着敲了一遍代码，将过程和不懂的记录了下来

--- Deeplearning/code/test.py
+++ Deeplearning/code/test.py
@@ -1,1 +1,17 @@
-print('hello Deep Learning!')
+# from functools import reduce
+# lst = [1, 2, 3, 4, 5]
+# a = [2, 2, 7, 4, 5]
+# # result = reduce(lambda x, y: x+y, lst, 1)
+# print(zip(list, a))
+
+# # print(result)   # 输出 120，即 1*2*3*4*5
+
+
+def square(x):
+    return x ** 2
+
+
+x = [1, 2, 3, 4]
+y = map(square, x)
+print(y)
+# 输出 [1, 4, 9, 16]
